96031e237597c1c0bc3e1470d405e30fec3b8645
Herjus <herjus420@gmail.com>
2023-04-06 18:08:00 +0200

    nvm

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -3,25 +3,31 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { MdNavigateNext, MdNavigateBefore } from 'react-icons/md'
 import { IconContext } from "react-icons";
-import { useEffect } from 'react';
-
-function reload(){
-  const view = document.querySelector('webview');
-  view.reload();
-}
-
-function GoBack(){
-  const view = document.querySelector('webview');
-
-  let canGoBack = view.canGoBack();
-  if (canGoBack) {
-    return <MdNavigateBefore color='white' size={"28"} className='hover:cursor-pointer'/>
-  } else {
-    return <MdNavigateBefore color='gray' size={"28"} className='hover:cursor-pointer'/>
-  }
-}
+import { useRef } from 'react';
 
 function Home() {
+
+  const view = useRef(null);
+
+  function reload(){
+    view.current.reload();
+  }
+  
+  function GoBack(){
+    function goBackFunc(){
+      view.current.goBack()
+    }
+    let canGoBack = view.current.canGoBack();
+      return <MdNavigateBefore onClick={() => goBackFunc()} color='white' size={"28"} className='hover:cursor-pointer'/>
+  }
+  function GoForward(){
+    function goForwardFunc(){
+      view.current.goForward()
+    }
+    let canGoBack = view.current.canGoBack();
+      return <MdNavigateNext onClick={() => goForwardFunc()} color='white' size={"28"} className='hover:cursor-pointer'/>
+  }
+
   return (
     <React.Fragment>
       <Head>
@@ -31,9 +37,7 @@ function Home() {
         <div className='w-full bg-gray-700 py-1 flex justify-between px-2 items-center text-sm'>
           <div className='flex items-center h-full'>
             <GoBack/>
-            <IconContext.Provider value={{ size: "28", style: { verticalAlign: "middle" } }}>
-              <MdNavigateNext className='hover:cursor-pointer'/>
-            </IconContext.Provider>
+            <GoForward/>
           </div>
           <div className='bg-black/50 px-2 rounded py-1 text-sm w-3/6 text-white'>
             <input placeholder='Search in google or type in an address' type="text" className='bg-transparent w-full border-none outline-none' />
@@ -42,7 +46,7 @@ function Home() {
             <span></span>
           </div>
         </div>
-        <webview src='https://google.com/' className='flex flex-1 flex-col'/>
+        <webview ref={view} src='https://google.com/' className='flex flex-1 flex-col'/>
       </div>
     </React.Fragment>
   );
