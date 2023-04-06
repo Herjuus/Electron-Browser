79708d025ce0d42f5a2de34264fedd8e82f44b27
Herjus <herjus420@gmail.com>
2023-04-06 17:39:01 +0200

    broken

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -3,6 +3,23 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { MdNavigateNext, MdNavigateBefore } from 'react-icons/md'
 import { IconContext } from "react-icons";
+import { useEffect } from 'react';
+
+function reload(){
+  const view = document.querySelector('webview');
+  view.reload();
+}
+
+function GoBack(){
+  const view = document.querySelector('webview');
+
+  let canGoBack = view.canGoBack();
+  if (canGoBack) {
+    return <MdNavigateBefore color='white' size={"28"} className='hover:cursor-pointer'/>
+  } else {
+    return <MdNavigateBefore color='gray' size={"28"} className='hover:cursor-pointer'/>
+  }
+}
 
 function Home() {
   return (
@@ -13,8 +30,8 @@ function Home() {
       <div className='h-screen flex flex-col'>
         <div className='w-full bg-gray-700 py-1 flex justify-between px-2 items-center text-sm'>
           <div className='flex items-center h-full'>
-            <IconContext.Provider value={{ color: "gray", size: "28", style: { verticalAlign: "middle" } }}>
-              <MdNavigateBefore className='hover:cursor-pointer'/>
+            <GoBack/>
+            <IconContext.Provider value={{ size: "28", style: { verticalAlign: "middle" } }}>
               <MdNavigateNext className='hover:cursor-pointer'/>
             </IconContext.Provider>
           </div>
